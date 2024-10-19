ac7973f911be9317dc51ef95b1e6d080e1be3754
ANTHONY8652 <githinjianthony720@gmail.com>
2024-10-19 19:02:52 +0300

    Refiews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from products.models import Product
+from users.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'user'], name='unique_product_user_review'),
+        ]
+
+    def __str__(self):
+        return f'Review for {self.product.name} by {self.user.username} -Rating: {self.rating}'
 
 # Create your models here.
