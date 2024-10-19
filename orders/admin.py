baeb5fa26cd214f5a94e7269f60a534fa71998a1
ANTHONY8652 <githinjianthony720@gmail.com>
2024-10-19 18:31:43 +0300

    admin panel updated

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Order
+from django.utils.translation import gettext_lazy as _
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'quantity', 'stock_status', 'ordered_at', 'get_order_status']
+    list_filter = ['orderes_at', 'user', 'product']
+    search_fields = ['user__username', 'product__name', 'ordered_at']
+    ordering = ['ordered_at']
+
+    def get_order_status(self, obj):
+        return obj.get_order_status()
+    
+    get_order_status.short_description = _('Order Status')
+
+    def stock_status(self, obj):
+        return 'In_Stock' if obj.product.stock_quantity >= obj.quantity else 'Insuficcient Stock' 
+    stock_status.short_description = _('Stock Status')
+
+    def mark_as_fulfilled(self, request, queryset):
+        for order in queryset:
+            if order.product.stock_quantity >= order.quantity:
+                order.product.stock_quantity -= order.quantity
+                order.product.save()
+                order.save()
+            
+            else:
+                self.message_user(request, f'Order {order.id} has insufficient stock.', level='error')
+            
+        self.message_user(request, 'Selected orders have been fulfilled.', level='success')
+        
+    mark_as_fulfilled.short_description = ('Mark selected orders as fulfilled.')
+
+    actions = [mark_as_fulfilled]
+
+
+admin.site.register(Order, OrderAdmin)
+
+
 
 # Register your models here.
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from products.models import Product
+from users.models import User
+from django.core.exceptions import ValidationError
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if self.quantity <= 0:
+            raise ValidationError('Quantity cannot be zero it must be greater than zero.')
+        
+        if self.product.stock_quantity < self.quantity:
+            raise ValidationError(f'Insufficient stock for {self.product} try a lower amount please.')
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        self.product.stock_quantity -= self.quantity
+        self.product.save()
+        super().save(*args, **kwargs)
+
+    def get_order_status(self):
+        if self.product.stock_quantity < self.quantity:
+            return (f'Insufficient stock, stock left is{self.product.stock_quantity}.')
+        
+        return ('In stock')
 
 # Create your models here.
